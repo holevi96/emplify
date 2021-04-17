858cfbd82e6f04a89f0ff5438a52ac5424ff66c7
lhorvath <levente.horvath@gravityrd.com>
2021-04-17 14:56:00 +0200

    adding functions.php

---
+++ functions.php
@@ -0,0 +1,20 @@
+function alter_the_edit_screen_query( $wp_query ) {
+    if ( strpos( $_SERVER[ 'REQUEST_URI' ], '/wp-admin/edit.php' ) !== false ) {
+        if ( !current_user_can( 'activate_plugins' ) )  {
+			add_action( 'views_edit-post', 'remove_items_from_edit' );
+            global $current_user;
+            $wp_query->set( 'author', $current_user->id );
+        }
+    }
+}
+
+add_filter('parse_query', 'alter_the_edit_screen_query' );
+
+function remove_items_from_edit( $views ) {
+    unset($views['all']);
+    unset($views['publish']);
+    unset($views['trash']);
+    unset($views['draft']);
+    unset($views['pending']);
+    return $views;
+}

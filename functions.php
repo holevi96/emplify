8afbe252591dfc4ced5d54e0d45cfb397de645af
lhorvath <levente.horvath@gravityrd.com>
2021-04-17 19:23:48 +0200

    set

--- functions.php
+++ functions.php
@@ -3,7 +3,6 @@
 function alter_the_edit_screen_query( $wp_query ) {
     if ( strpos( $_SERVER[ 'REQUEST_URI' ], '/wp-admin/edit.php' ) !== false ) {
         if ( !current_user_can( 'activate_plugins' ) )  {
-			add_action( 'views_edit-post', 'remove_items_from_edit' );
             global $current_user;
             $wp_query->set( 'author', $current_user->id );
         }
@@ -11,15 +10,6 @@ function alter_the_edit_screen_query( $wp_query ) {
 }
 
 add_filter('parse_query', 'alter_the_edit_screen_query' );
-
-function remove_items_from_edit( $views ) {
-    unset($views['all']);
-    unset($views['publish']);
-    unset($views['trash']);
-    unset($views['draft']);
-    unset($views['pending']);
-    return $views;
-}
 
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {

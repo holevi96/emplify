6f560088acc5f8636d8283c4e76cf59021897f85
lhorvath <levente.horvath@gravityrd.com>
2021-04-17 15:10:13 +0200

    generate title

--- functions.php
+++ functions.php
@@ -25,3 +25,14 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+add_filter( 'wp_insert_post_data' , 'modify_post_title' , '99', 1 ); // Grabs the inserted post data so you can modify it.
+function modify_post_title( $data )
+{
+  if($data['post_type'] == 'esetek') { // If the actual field name of the rating date is different, you'll have to update this.
+    $date = date("Ymd");
+    $title = $date;
+    $data['post_title'] =  $title ; //Updates the post title to your new title.
+  }
+  return $data; // Returns the modified data.
+}

12513f1c7a1d9f11ce007475342a4ff01daa986a
lhorvath <levente.horvath@gravityrd.com>
2021-04-24 21:58:37 +0200

    jo

--- functions.php
+++ functions.php
@@ -55,26 +55,14 @@ function validate_empty_fields( $valid, $value, $field, $input ){
 	
 }
 
- /*add_filter('acf/update_value/name=maximum_alkalmak_szama', 'my_acf_update_value', 10, 4);
- function my_acf_update_value( $value, $post_id, $field, $original=0 ) {
-  $ceg_id = $_POST['acf']['field_607c123c2d4f8'];
-  $max = get_field('face_to_face_maximum_appointment', $ceg_id);
-  return (int)$max;
-}*/
-
-/*function sync_acf($post_id, $post, $update) {
-    
-  $ceg_id = get_field('munkavallalo_cege', $post_id); // NOTE: enter the name of the ACF field here
-  $max = get_field('face_to_face_maximum_appointment', $ceg_id);
-  update_field('face_to_face_maximum_appointment', 10, $post_id);
-}
-add_action('save_post', 'sync_acf', 10, 3);*/
 
 add_action('acf/save_post', 'save_post_functions', 11);
 
 function save_post_functions( $post_id ) {
   $ceg_id = get_field('munkavallalo_cege', $post_id)[0]; // NOTE: enter the name of the ACF field here
   $max = get_field('face_to_face_maximum_appointment', $ceg_id);
+  $tanacsadas_datumai = get_field('tanacsadas_datumai', $post_id);
 
+  update_field('lezajlott_alkalmak_szama', count($tanacsadas_datumai), $post_id);
   update_field('maximum_alkalmak_szama', (int)$max, $post_id);
 }
